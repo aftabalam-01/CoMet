f7c3f45d199a713160b0a0cc355ffb058c1c94af
aftabalam-01 <aftabaaa0@gmail.com>
2025-06-14 18:24:21 +0530

    orders completed

--- src/controllers/order.ts
+++ src/controllers/order.ts
@@ -46,3 +46,85 @@ export const viewOrder = asyncHandler(async (req: Request, res: Response) => {
     throw new Error(error);
   }
 });
+
+export const viewOrders = asyncHandler(async (req: Request, res: Response) => {
+  const orders = await prisma.order.findMany();
+  res.status(200).json({ orders });
+});
+
+export const updateOrder = asyncHandler(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const orderId = parseInt(id);
+  const {
+    shopId,
+    boxes,
+    billno,
+    note,
+    transportId,
+    createdById,
+    deliveryStatus,
+    payment,
+  } = req.body;
+
+  const findOrder = await prisma.order.findUnique({
+    where: {
+      id: orderId,
+    },
+  });
+  if (!findOrder) {
+    res.status(404).json("Order not Found");
+    return;
+  }
+  const updatedOrder = await prisma.order.update({
+    where: { id: orderId },
+    data: {
+      shopId,
+      boxes,
+      billno,
+      note,
+      transportId,
+      createdById,
+      deliveryStatus,
+      payment,
+    },
+  });
+  res.status(200).json({ updatedOrder });
+});
+
+export const deleteOrder = asyncHandler(async (req: Request, res: Response) => {
+  const id = req.params.id;
+  const orderId = parseInt(id);
+
+  const findOrder = await prisma.order.findUnique({
+    where: {
+      id: orderId,
+    },
+  });
+  if (!findOrder) {
+    res.status(404).json("Order not Found");
+    return;
+  }
+  const deletedOrder = await prisma.order.delete({
+    where: { id: orderId },
+  });
+  res.status(200).json({ deletedOrder });
+});
+
+export const deleteBulkOrders = asyncHandler(
+  async (req: Request, res: Response) => {
+    const ids = req.body.ids;
+
+    if (!Array.isArray(ids) || ids.length == 0) {
+      res.status(400);
+      throw new Error("Please provide an array of shop IDs to delete.");
+    }
+    const deleteBulk = await prisma.order.deleteMany({
+      where: {
+        id: {
+          in: ids,
+        },
+      },
+    });
+    res.status(200).json(`${deleteBulk.count} Orders Deleted Successfully`);
+  }
+);
--- src/routes/order.ts
+++ src/routes/order.ts
@@ -1,10 +1,21 @@
 import { Router } from "express";
 import { authMiddleware, isAdmin } from "../middlewares/authMiddleware";
-import { createOrder, viewOrder } from "../controllers/order";
+import {
+  createOrder,
+  deleteBulkOrders,
+  deleteOrder,
+  updateOrder,
+  viewOrder,
+  viewOrders,
+} from "../controllers/order";
 
 const router = Router();
 
-router.post("/create", createOrder);
-router.get("/order-detail/:id", viewOrder);
+router.post("/create", authMiddleware, isAdmin, createOrder);
+router.get("/order-detail/:id", authMiddleware, isAdmin, viewOrder);
+router.get("/orders", authMiddleware, isAdmin, viewOrders);
+router.put("/order-update/:id", authMiddleware, isAdmin, updateOrder);
+router.delete("/order-delete/:id", authMiddleware, isAdmin, deleteOrder);
+router.delete("/orders-delete", authMiddleware, isAdmin, deleteBulkOrders);
 
 export default router;
